194b96ec114f162a187fe7e63c1adbc995cdfd7d
unknown <rajan23024@gmail.com>
2023-06-03 16:25:54 +0530

    minor changes in doubt

--- pages/api/rsgt/doubts.js
+++ pages/api/rsgt/doubts.js
@@ -11,18 +11,18 @@ export default async function handler(req, res) {
   const body = { teacher, subject, title, description, name, mobile };
   try {
     const db = await connectToDatabase();
-    const collection = db.collection('users');
+    const collection = db.collection('doubt');
 
-    // Store the signup data in the database
+    // Store the doubt data in the database
     await collection.insertOne(body);
 
-    // Send the email with the signup details
-    const subject = 'Signup Details';
+    // Send the email with the doubt details
+    const subject = 'Doubt Details';
     // const body = `Name: ${name}\nEmail: ${email}\nPassword: ${mobileNumber}`;
     const htmlBody = getHtmlBody(body)
     await sendEmail('freshmorning1462@gmail.com', subject, null, htmlBody);
 
-    res.status(201).json({ message: 'Signup successful' });
+    res.status(201).json({ message: 'doubt successful' });
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: 'Error occurred' });
